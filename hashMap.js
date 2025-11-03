1e2b8c32b803ff044c889d3f5586e5c7ce64efc8
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-03 22:13:58 +0100

    Implement values & entries mathods

--- hashMap.js
+++ hashMap.js
@@ -133,6 +133,31 @@ class HashMap {
 
     return mapKeys;
   }
+
+  values() {
+    let mapValues = [];
+
+    for (let bucket of this.map) {
+      if (!bucket) continue;
+      for (let slot of bucket) {
+        mapValues.push(slot[1]);
+      }
+    }
+
+    return mapValues;
+  }
+
+  entries() {
+    let mapEntries = [];
+    for (let bucket of this.map) {
+      if (!bucket) continue;
+      for (let slot of bucket) {
+        mapEntries.push([slot[0], slot[1]]);
+      }
+    }
+
+    return mapEntries;
+  }
 }
 
 const test = new HashMap();
