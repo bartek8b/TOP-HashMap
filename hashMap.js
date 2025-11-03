c7fdca82be4e4e995066c6838aaf7572ef9480f9
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-03 15:34:37 +0100

    Implement this.man Array, add hash method

--- hashMap.js
+++ hashMap.js
@@ -2,5 +2,21 @@ class HashMap {
   constructor(capacity = 16, loadFactor = 0.75) {
     this.capacity = capacity;
     this.loadFactor = loadFactor;
+    this.map = new Array(capacity);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode % this.capacity;
   }
 }
+
+const test = new HashMap();
+
+console.log(test.hash('Balcerzak'));
