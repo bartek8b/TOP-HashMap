e57a38a6451f05def2f702983aa0f8311e6761a3
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-03 17:21:56 +0100

    Set set() method

--- hashMap.js
+++ hashMap.js
@@ -15,8 +15,28 @@ class HashMap {
 
     return hashCode % this.capacity;
   }
+
+  set(key, value) {
+    const index = this.hash(key);
+    // If the bucket is empty
+    if (!this.map[index]) {
+      this.map[index] = [[key, value]];
+      return [key, value];
+    }
+    // If the bucket is not empty
+    const bucket = this.map[index];
+    for (let elem of bucket) {
+      if (elem[0] === key) {
+        elem[1] = value;
+        return elem;
+      }
+    }
+    // If there is no such key in the bucket
+    bucket.push([key, value]);
+  }
 }
 
 const test = new HashMap();
 
-console.log(test.hash('Balcerzak'));
+test.set('apple', 'red');
+console.log(test);
